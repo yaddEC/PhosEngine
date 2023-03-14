8e0010e67e4f841805dc502beb429e81e7191517
Laurent LIN <l.lin@student.isartdigital.com>
2023-03-14 09:50:39 +0100

    add external resource

---
+++ External/PhosCore/IResource.hpp
@@ -0,0 +1,36 @@
+#pragma once
+#include <string>
+
+#ifdef IRESOURCE_EXPORTS
+#define IRESOURCE_API __declspec(dllexport)
+#else
+#define IRESOURCE_API __declspec(dllimport)
+#endif
+
+namespace Resource
+{
+	class IRESOURCE_API IResource
+	{
+	public:
+
+		IResource() { }
+
+		virtual void Load(const std::string& filepath) = 0;
+		virtual void Bind() = 0;
+		virtual void Unload() = 0;
+
+		inline bool GetIsLoaded() { return p_isLoaded; }
+
+		// Do not call every frame
+		virtual class Texture* GenerateFileIcon() = 0;
+
+	protected:
+
+		bool p_isLoaded = false;
+
+		std::string p_directory;
+		std::string p_name;
+		
+		void SetFileInfo(const std::string& filepath);
+	};
+}
---
+++ External/PhosCore/Texture.hpp
@@ -0,0 +1,46 @@
+#pragma once
+#include "IResource.hpp"
+
+#ifdef TEXTURE_EXPORTS
+#define TEXTURE_API __declspec(dllexport)
+#else
+#define TEXTURE_API __declspec(dllimport)
+#endif
+
+namespace Resource
+{
+	class TEXTURE_API Texture : public IResource
+	{
+	public:
+
+		Texture(unsigned char* _data = nullptr, int _width = 1, int _height = 1, int _nrChannels = 3);
+		~Texture() { Unload(); }
+
+		void Load(const std::string& filepath) override;
+		void SetData(unsigned char* _data, int _width, int _height, int _nrChannel = 3);
+
+		void Bind() override;
+		void Unload() override;
+
+
+		unsigned int GetTextureKey() const { return m_textureKey; }
+
+		unsigned int GetTextureWidth() const { return m_width; }
+		unsigned int GetTextureHeight() const { return m_height; }
+
+		//float GetTextureSizeRatio() { return (float)width / (float)height; }
+
+		void ResizeAndReset(int width, int height);
+
+		void DisplayImage(float maxSize);
+
+		Texture* GenerateFileIcon() override;
+
+	private:
+
+		unsigned int m_textureKey;
+		int m_width, m_height;
+		unsigned char* m_data;
+		int m_nrChannels;
+	};
+}
