2136fecb101c49619fb94a90a92509e369e91560
Laurent LIN <l.lin@student.isartdigital.com>
2023-04-19 09:14:26 +0200

    modify material class for pbr

--- Sources/Engine/Scene.cpp
+++ Sources/Engine/Scene.cpp
@@ -36,10 +36,12 @@ Scene::Scene()
 	Mesh* boo = Resource::ResourceManager::GetInstance().GetResource<Mesh>("Assets\\Model\\boo.obj");
 	Mesh* ground = Resource::ResourceManager::GetInstance().GetResource<Mesh>("Assets\\Model\\cube.obj");
 	Mesh* blaziken = Resource::ResourceManager::GetInstance().GetResource<Mesh>("Assets\\Model\\blaziken.obj");
+	Mesh* sphere = Resource::ResourceManager::GetInstance().GetResource<Mesh>("DefaultAssets\\Model\\primitiveSphere.obj");
 
 	Material* booMat = Resource::ResourceManager::GetInstance().GetResource<Material>("Assets\\Material\\boo_mat.001.phmat");
 	Material* blazikenMat = Resource::ResourceManager::GetInstance().GetResource<Material>("Assets\\Material\\Blaziken.phmat");
 	Material* basicMat = Resource::ResourceManager::GetInstance().GetResource<Material>("Assets\\Material\\Material.phmat");
+	Material* metalGrid = Resource::ResourceManager::GetInstance().GetResource<Material>("Assets\\Material\\MetalGrid.phmat");
 
 	GameObject* go = new GameObject();
 	go->name = "Boo 1";
@@ -49,8 +51,8 @@ Scene::Scene()
 	go->transform->position.y = 10;
 
 
-	Rigidbody* rb = go->AddComponent<Rigidbody>();
-	SphereCollider* col = go->AddComponent<SphereCollider>();
+	//Rigidbody* rb = go->AddComponent<Rigidbody>();
+	//SphereCollider* col = go->AddComponent<SphereCollider>();
 	
 
 	
@@ -74,7 +76,14 @@ Scene::Scene()
 	rend3->SetMaterial(basicMat);
 	go3->transform->position.y = -3;
 	go3->transform->scale = Maths::Vec3(50, 1, 100);
-	BoxCollider* col2 = go3->AddComponent<BoxCollider>();
+	//BoxCollider* col2 = go3->AddComponent<BoxCollider>();
+
+	GameObject* go4 = new GameObject();
+	go4->name = "testMat";
+	MeshRenderer* rend4 = go4->AddComponent<MeshRenderer>();
+	rend4->SetMesh(sphere);
+	rend4->SetMaterial(metalGrid);
+
 
 	//GameObject* light = new GameObject();
 	//DirectionalLight* dirLight = light->AddComponent<LowRenderer::DirectionalLight>();
@@ -92,12 +101,13 @@ Scene::Scene()
 	Instantiate(go);
 	Instantiate(go2);
 	Instantiate(go3);
+	Instantiate(go4);
 	//Instantiate(light);
 	//Instantiate(light1);
 	Instantiate(light2);
 
-	col->Setup(Maths::Vec3(0, 0, 0), Maths::Vec3(2.5, 2.5, 2.5), false, Wrapper::BOUNCY_BALL);
-	col2->Setup(Maths::Vec3(0, 0, 0), Maths::Vec3(1, 1, 1), false, Wrapper::BOUNCY_BALL);
+	//col->Setup(Maths::Vec3(0, 0, 0), Maths::Vec3(2.5, 2.5, 2.5), false, Wrapper::BOUNCY_BALL);
+	//col2->Setup(Maths::Vec3(0, 0, 0), Maths::Vec3(1, 1, 1), false, Wrapper::BOUNCY_BALL);
 }
 
 void Scene::GameObjectFromBuffer()
--- Sources/Wrapper/RHI.cpp
+++ Sources/Wrapper/RHI.cpp
@@ -105,6 +105,8 @@ void RHI::BindTexture(unsigned int* textureKey, unsigned char* data, int channel
 
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
 
 	if (data)
 	{
