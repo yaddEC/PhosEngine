af834c5fa0712b4ce3ef0b74e69cc08e9c5b30d8
Yann DECHAUX <y.dechaux@student.isartdigital.com>
2023-05-15 16:34:23 +0200

    [FIX] Fixed the physics menu

--- Sources/GUI/EditorGUI/PhysicsSettingsGUI.cpp
+++ Sources/GUI/EditorGUI/PhysicsSettingsGUI.cpp
@@ -28,27 +28,22 @@ void EditorGUI::PhysicsSettingsGUI::DoUpdate()
     GUI::BeginGroup();
     GUI::DisplayText("New Layer Name");
     GUI::InputString(" ", newLayerName,false);
-
     if (GUI::Button("Create Layer") && newLayerName !="" && newLayerName.c_str()[0] != ' ')
     {
         Wrapper::Physics::CreateLayer(newLayerName);
         newLayerName = "";
     }
     GUI::EndGroup();
-
     std::vector<std::string> layernames = *Wrapper::Physics::GetLayerNames();
     if(layernames.size()>0)
     { 
     GUI::BeginGroup();
     GUI::BeginGroup();
-
     GUI::DisplayText("\n \n");
-   
     GUI::EndGroup();
     for (int i = 0; i < layernames.size(); i++)
     {
     GUI::SameLine();
-
     GUI::BeginGroup();
     
         GUI::DisplayText("%s\n\n", layernames[i].c_str());
