5746fd594d0b4f97448c8e3086f049c72b7f2b0a
Laurent LIN <l.lin@student.isartdigital.com>
2023-05-30 19:12:34 +0200

    add input manager

--- External/PhosEngine/Script/InputManager.hpp
+++ External/PhosEngine/Script/InputManager.hpp
@@ -2,29 +2,29 @@
 
 #include "Engine/MonoBehaviour.hpp"
 
+namespace Engine
+{
+	class GamepadInput;
+}
+
 namespace Script
 {
-	class PHOSENGINE_API PlayerMouvement : public Engine::MonoBehaviour
+	class PlayerMouvement;
+
+	class PHOSENGINE_API InputManager : public Engine::MonoBehaviour
 	{
 	public:
-		PlayerMouvement();
-		~PlayerMouvement();
+		InputManager();
+		~InputManager();
 		void Start() override;
 		void Update() override;
 		void GUIUpdate() override;
 		void OnInspector() override;
 		void OnDestroy() override;
-		void SetIdUser(int ID);
 		Reflection::ClassMetaData& GetMetaData() override;
 	private:
-		float m_playerSpeed;
-		int m_idUser;
-		int user; //temp
-		bool up;
-		bool right;
-		bool down;
-		bool left;
-
+		Engine::GamepadInput* gamepads;
+		std::vector<PlayerMouvement*> playerMovement;
 	};
 
 
