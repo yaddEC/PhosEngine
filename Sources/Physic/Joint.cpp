b6672aa9aba172785670b07a3f7bf716359a1ea0
Yann DECHAUX <y.dechaux@student.isartdigital.com>
2023-05-30 15:05:13 +0200

    [ADD] Small joints progress

--- Sources/Physic/Joint.cpp
+++ Sources/Physic/Joint.cpp
@@ -140,9 +140,53 @@ Reflection::ClassMetaData& Physic::ConfigurableJoint::GetMetaData()
         result.name = "Configurable Joint";
         result.memberList = {
             ClassMemberInfo("Collider", offsetof(ConfigurableJoint, p_gameObjectId), MemberType::T_GAME_OBJECT),
+            ClassMemberInfo("Axis", offsetof(ConfigurableJoint, m_anchor), MemberType::T_VEC3),
+            ClassMemberInfo("Anchor", offsetof(ConfigurableJoint, m_anchor), MemberType::T_VEC3),
+            ClassMemberInfo("Connect_Anchor", offsetof(ConfigurableJoint, m_connectedAnchor), MemberType::T_VEC3),
+            ClassMemberInfo("Motion", offsetof(ConfigurableJoint, m_motion), MemberType::T_VEC3),
+            ClassMemberInfo("Ang._Motion", offsetof(ConfigurableJoint, m_angularMotion), MemberType::T_VEC3),
 
-            ClassMemberInfo("BreakForce", offsetof(ConfigurableJoint, p_breakForce), MemberType::T_INF_FLOAT),
-            ClassMemberInfo("BreakTorque", offsetof(ConfigurableJoint, p_breakTorque), MemberType::T_INF_FLOAT),
+            ClassMemberInfo("Lin.Lim.Spring_Damp", offsetof(ConfigurableJoint, m_linearLimitSpring.damper), MemberType::T_FLOAT),
+            ClassMemberInfo("Lin.Lim.Spring_Spring", offsetof(ConfigurableJoint, m_linearLimitSpring.spring), MemberType::T_FLOAT),
+
+            ClassMemberInfo("Lin_Limit", offsetof(ConfigurableJoint, m_linearLimit.limit), MemberType::T_FLOAT),
+            ClassMemberInfo("Lin_Limit_Bounciness", offsetof(ConfigurableJoint, m_linearLimit.bounciness), MemberType::T_FLOAT),
+            ClassMemberInfo("Lin_Limit_Contact_Dist", offsetof(ConfigurableJoint, m_linearLimit.contactDistance), MemberType::T_FLOAT),
+
+            ClassMemberInfo("XDrive_Max_Force ", offsetof(ConfigurableJoint, m_xDrive.maximumForce), MemberType::T_FLOAT),
+            ClassMemberInfo("XDrive_Pos_Damper", offsetof(ConfigurableJoint, m_xDrive.positionDamper), MemberType::T_FLOAT),
+            ClassMemberInfo("XDrive_Pos_Spring", offsetof(ConfigurableJoint, m_xDrive.positionSpring), MemberType::T_FLOAT),
+
+            ClassMemberInfo("YDrive_Max_Force ", offsetof(ConfigurableJoint, m_yDrive.maximumForce), MemberType::T_FLOAT),
+            ClassMemberInfo("YDrive_Pos_Damper", offsetof(ConfigurableJoint, m_yDrive.positionDamper), MemberType::T_FLOAT),
+            ClassMemberInfo("YDrive Pos Spring", offsetof(ConfigurableJoint, m_yDrive.positionSpring), MemberType::T_FLOAT),
+
+            ClassMemberInfo("ZDrive_Max_Force ", offsetof(ConfigurableJoint, m_zDrive.maximumForce), MemberType::T_FLOAT),
+            ClassMemberInfo("ZDrive_Pos_Damper", offsetof(ConfigurableJoint, m_zDrive.positionDamper), MemberType::T_FLOAT),
+            ClassMemberInfo("ZDrive Pos Spring", offsetof(ConfigurableJoint, m_zDrive.positionSpring), MemberType::T_FLOAT),
+
+            ClassMemberInfo("Ang_XDrive_Max_Force", offsetof(ConfigurableJoint, m_angularXDrive.maximumForce), MemberType::T_FLOAT),
+            ClassMemberInfo("Ang_XDrive_Pos_Damper", offsetof(ConfigurableJoint, m_angularXDrive.positionDamper), MemberType::T_FLOAT),
+            ClassMemberInfo("Ang_XDrive_Pos_Spring", offsetof(ConfigurableJoint, m_angularXDrive.positionSpring), MemberType::T_FLOAT),
+
+            ClassMemberInfo("Ang_YDrive_Max_Force ", offsetof(ConfigurableJoint, m_angularYDrive.maximumForce), MemberType::T_FLOAT),
+            ClassMemberInfo("Ang_YDrive_Pos_Damper", offsetof(ConfigurableJoint, m_angularYDrive.positionDamper), MemberType::T_FLOAT),
+            ClassMemberInfo("Ang_YDrive_Pos_Spring", offsetof(ConfigurableJoint, m_angularYDrive.positionSpring), MemberType::T_FLOAT),
+
+            ClassMemberInfo("Ang_ZDrive_Max_Force ", offsetof(ConfigurableJoint, m_angularZDrive.maximumForce), MemberType::T_FLOAT),
+            ClassMemberInfo("Ang_ZDrive_Pos_Damper", offsetof(ConfigurableJoint, m_angularZDrive.positionDamper), MemberType::T_FLOAT),
+            ClassMemberInfo("Ang_ZDrive_Pos_Spring", offsetof(ConfigurableJoint, m_angularZDrive.positionSpring), MemberType::T_FLOAT),
+
+            ClassMemberInfo("Ang_YZDrive_Max_Force ", offsetof(ConfigurableJoint, m_angularZDrive.maximumForce), MemberType::T_FLOAT),
+            ClassMemberInfo("Ang_YZDrive_Pos_Damper", offsetof(ConfigurableJoint, m_angularZDrive.positionDamper), MemberType::T_FLOAT),
+            ClassMemberInfo("Ang_YZDrive_Pos_Spring", offsetof(ConfigurableJoint, m_angularZDrive.positionSpring), MemberType::T_FLOAT),
+
+            ClassMemberInfo("Ang._Motion", offsetof(ConfigurableJoint, m_angularMotion), MemberType::T_VEC3),
+
+
+            ClassMemberInfo("Target_Pos", offsetof(ConfigurableJoint, m_targetPosition), MemberType::T_INF_FLOAT),
+            ClassMemberInfo("Target_Velocity", offsetof(ConfigurableJoint, m_targetVelocity), MemberType::T_INF_FLOAT),
+            ClassMemberInfo("Target_Ang_Velocity", offsetof(ConfigurableJoint, m_targetAngularVelocity), MemberType::T_INF_FLOAT),
         };
         computed = true;
     }
