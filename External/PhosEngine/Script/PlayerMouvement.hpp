77524186b5cb2fd9d7fe02e07c02cf34a179fe52
Laurent LIN <l.lin@student.isartdigital.com>
2023-05-30 11:11:54 +0200

    add keyboard controle

---
+++ External/PhosEngine/Script/PlayerMouvement.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "Engine/MonoBehaviour.hpp"
+
+namespace Script
+{
+	class PHOSENGINE_API PlayerMouvement : public Engine::MonoBehaviour
+	{
+	public:
+		PlayerMouvement();
+		~PlayerMouvement();
+		void Start() override;
+		void Update() override;
+		void GUIUpdate() override;
+		void OnInspector() override;
+		void OnDestroy() override;
+		Reflection::ClassMetaData& GetMetaData() override;
+	private:
+		float m_playerSpeed;
+	};
+
+
+}
