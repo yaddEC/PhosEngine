eb375f6a688d92dc3a17854ab841fb333f928c1e
ROBION Mathieu <m.robion@student.isartdigital.com>
2023-05-30 11:31:14 +0200

    [update]Sound updated

--- External/PhosEngine/Sound/SoundPlayer.hpp
+++ External/PhosEngine/Sound/SoundPlayer.hpp
@@ -12,7 +12,6 @@ namespace Sound
 		SoundPlayer();
 		~SoundPlayer();
 
-		Resource::Audio* audio;
 
 		void Update() override;
 		void Start() override;
@@ -20,7 +19,9 @@ namespace Sound
 		void GUIUpdate() override;
 		void OnDestroy() override;
 		Reflection::ClassMetaData& GetMetaData() override;
-	protected:
-		bool p_isLooping;
+	private:
+		bool m_isLooping;
+		Resource::Audio* m_audio;
+		float m_volume;
 	};
 }
