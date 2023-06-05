8f96a3c731cfa9b6156f72950a5c039f7de68b76
Liam ZALLIO <l.zallio@student.isartdigital.com>
2023-06-05 15:42:53 +0200

    player movement physics NEED FREEZE ROTATION

--- Sources/Wrapper/RHI.cpp
+++ Sources/Wrapper/RHI.cpp
@@ -42,7 +42,7 @@ void RHI::BindTexture(unsigned int* textureKey, unsigned char* data, int channel
 		if (channel == 4)
 			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
 		if (channel == 3)
-			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
 	}
 	else
 	{
